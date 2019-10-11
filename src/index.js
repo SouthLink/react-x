0412a0efc9d6c62c00635facd36615aca73c2701
biao.tang <biao.tang@xuncetech.com>
2019-10-11 18:53:00 +0800

    react init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+moment.locale('zh-cn');
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/pages/login.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import loginBg from '../assets/login-bg.jpg';
+
+export default class login extends Component {
+  constructor(props, context) {
+    super(props);
+    
+  }
+}
